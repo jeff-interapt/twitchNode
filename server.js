dcaf3ec94b29d12e2ff4fc3a7c459e1858d94a18
jeff.reed@kindred.com <jeff.reed@kindred.com>
2020-09-21 12:16:49 -0400

    Fixed Server.js

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const request = require("request");
 const async = require("async");
 var accessToken = '';
+
+
+function gameRequest(accessToken){
+    setTimeout(() => {
+    const gameOptions = {
+        url: 'https://api.twitch.tv/helix/games/top',
+        method: 'GET',
+        headers:{
+            'Client-ID': 'w90oqmu9u0of2vsh0xnv6qxivn8bko',
+            'Authorization': 'Bearer ' + accessToken
+        }
+    }
+    if(!accessToken){
+        console.log("No Token");
+    }else{
+        console.log(gameOptions);
+    
+    const gameRequest = request.get(gameOptions,(err,res,body) => {
+        if(err){
+            return console.log(err);
+        }
+        
+        console.log('Status: ${res.statusCode}');
+        console.log(JSON.parse(body));
+    });
+    
+    };
+    
+    },2000)
+    }
 const options = {
     url: 'https://id.twitch.tv/oauth2/token',
     json:true,
@@ -22,29 +52,8 @@ request.post(options, (err,res,body)=>{
     }
     console.log('Status: ${res.statusCode}');
     console.log(body.access_token);
-    return  accessToken = body.access_token;
+    gameRequest(body.access_token);
     
 });
 
 
-
-
-const gameOptions = {
-    url: 'https://api.twitch.tv/helix/games/top',
-    json:true,
-    client_id: 'w90oqmu9u0of2vsh0xnv6qxivn8bko',
-    Authorization: 'Bearer ' + accessToken,
-    timeout: 5000
-}
-if(!accessToken){
-    console.log("No Token");
-}else{
-request.get(gameOptions,(err,res,body) => {
-    if(err){
-        return console.log(err);
-    }
-    console.log('Status: ${res.statusCode}');
-    console.log(body);
-});
-
-};
